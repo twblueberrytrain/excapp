68b8db4bc97ec3470324ef6f972d823e4cc397e3
twblueberrytrain <louisgundam@gmail.com>
2018-08-22 20:56:05 +0800

    add string

--- app/src/main/java/com/exc/louis/itunesapp/util/Constant.java
+++ app/src/main/java/com/exc/louis/itunesapp/util/Constant.java
@@ -2,4 +2,5 @@ package com.exc.louis.itunesapp.util;
 
 public class Constant {
     public static final String EB_RESPONSE_SONG_LIST = "res_song_list";
+    public static final String EB_RESPONSE_COVER_IMAGE = "res_cover_image";
 }
