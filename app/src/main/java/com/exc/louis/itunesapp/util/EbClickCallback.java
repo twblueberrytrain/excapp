032398226e5d18332dee44bf500714ec081a93df
twblueberrytrain <louisgundam@gmail.com>
2018-08-22 23:42:34 +0800

    add click callback

---
+++ app/src/main/java/com/exc/louis/itunesapp/util/EbClickCallback.java
@@ -0,0 +1,14 @@
+package com.exc.louis.itunesapp.util;
+
+import android.view.View;
+
+public class EbClickCallback {
+    View view;
+    public EbClickCallback(View view) {
+        this.view = view;
+    }
+
+    public View getView() {
+        return view;
+    }
+}
