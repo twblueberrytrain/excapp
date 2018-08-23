3b2472157570f2ffe80e276158c7e0db51af860a
twblueberrytrain <louisgundam@gmail.com>
2018-08-23 22:11:46 +0800

    remove variable type

--- app/src/main/java/com/exc/louis/itunesapp/util/QuerySetting.java
+++ app/src/main/java/com/exc/louis/itunesapp/util/QuerySetting.java
@@ -3,12 +3,10 @@ package com.exc.louis.itunesapp.util;
 public class QuerySetting {
     private String keyWord;
     private int limit;
-    private String type;
 
     public QuerySetting(String keyWord) {
         this.keyWord = keyWord;
         limit = 20;
-        type = "all";
     }
 
     public String getKeyWord() {
@@ -21,13 +19,5 @@ public class QuerySetting {
 
     public void setLimit(int limit) {
         this.limit = limit;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
     }
 }
